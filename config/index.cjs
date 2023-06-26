311630c84c7309c7efd83ec64354bd38e0a45fca
Sander97 <80102774+Sander-97@users.noreply.github.com>
2023-06-26 20:42:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -106,17 +106,21 @@ const USER_CONFIG = {
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-18',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '本周',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://tophub.today/n/KqndgxeLl9',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1998', date: '09-09',
-        },
+        // {
+        //   type: '*生日', name: '宝贝', year: '1998', date: '09-09',
+        // },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1998', date: '08-18',
+          type: '生日', name: '宝贝', year: '1998', date: '08-18',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '05-09',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -124,20 +128,20 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-05-09' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU23646TURh3qbsIxeHWYxIQgKwzrHKX4QUaoMWh',
     }
   ],
 
